7aafc45f1cb3cde39850c30472255ed3a8680c24
Sunghyun01 <37784262+Sunghyun01@users.noreply.github.com>
2022-12-12 17:54:52 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,43 +1,51 @@
 package hello.core.scope;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
-import static org.assertj.core.api.Assertions.*;
-public class PrototypeTest {
-    @Test
-    public void prototypeBeanFind() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-        System.out.println("find prototypeBean1");
+import static org.assertj.core.api.Assertions.assertThat;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonWithPrototypeTest1 {
+    
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+       
         PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        System.out.println("find prototypeBean2");
+        prototypeBean1.addCount();
+        
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
 
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
 
-        System.out.println("prototypeBean1 = " + prototypeBean1);
-        System.out.println("prototypeBean2 = " + prototypeBean2);
-
-        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
-
-        ac.close(); //종료
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
     }
-
+    
     @Scope("prototype")
     static class PrototypeBean {
+        
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
 
         @PostConstruct
         public void init() {
-            System.out.println("PrototypeBean.init");
+            System.out.println("PrototypeBean.init " + this);
         }
         @PreDestroy
         public void destroy() {
             System.out.println("PrototypeBean.destroy");
         }
-        
     }
 }
